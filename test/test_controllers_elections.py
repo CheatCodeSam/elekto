cc2f4d803d8df449e5ea98e4eb51fd621e68efc4
Carson Weeks <mail@carsonweeks.com>
2025-02-05 18:20:54 -0500

    Created test for dashboard

--- test/test_controllers_elections.py
+++ test/test_controllers_elections.py
@@ -1,8 +1,10 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from flask.testing import FlaskClient
 import pytest
+from pytest_mock import MockerFixture
 
 from elekto import APP, SESSION, constants
+from elekto.models import meta
 from elekto.models.sql import User, drop_all, migrate
 
 @pytest.fixture(scope="module", autouse=True)
@@ -39,3 +41,22 @@ def test_unauthenticated_dashboard(client: FlaskClient):
         session[constants.AUTH_STATE] = None
     response = client.get("/app")
     assert response.status_code == 302
+
+def test_elections_running_dashboard(client: FlaskClient, mocker: MockerFixture):
+    mocker.patch("elekto.meta.Election.where", return_value=[{"name": "Demo Election", 
+                                                              "organization": "kubernetes", 
+                                                              "start_datetime": datetime.min, 
+                                                              "end_datetime": datetime.max}])
+    token="token"
+    with APP.app_context():
+        SESSION.add(User(username="carson",
+                             name="Carson Weeks",
+                             token=token,
+                             token_expires_at=datetime.max))
+        SESSION.commit()
+    with client.session_transaction() as session:
+        session[constants.AUTH_STATE] = token
+    response = client.get("/app")
+    assert response.status_code == 200
+    assert b"Demo Election" in response.data 
+    assert not b"Sit back and Relax, there is not to do yet." in response.data
